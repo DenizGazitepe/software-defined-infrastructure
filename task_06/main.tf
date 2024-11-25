95080964654950560600883175f378fc3bd1d903
Ingodongo <sauter-nico@gmx.de>
2024-11-25 15:04:35 +0100

    Added Yaml call

--- task_06/main.tf
+++ task_06/main.tf
@@ -8,6 +8,7 @@ terraform {
   required_version = ">= 1.0"
 }
 
+# Setup the firewall
 resource "hcloud_firewall" "sshFw" {
   name = "ssh-firewall"
   rule {
@@ -17,7 +18,7 @@ resource "hcloud_firewall" "sshFw" {
     source_ips = ["0.0.0.0/0", "::/0"]
   }
 }
-
+# Add the ssh keys
 resource "hcloud_ssh_key" "loginDeniz" {
   name       = "deniz@LegionSlim7-Deniz"
   public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICF0ll5wZPXwKvIqo9Dvp0bLJVu54pvHhji6i73pgiOE"
@@ -33,7 +34,6 @@ resource "hcloud_ssh_key" "loginNico" {
   public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH9FnrukU1in+njcoOtPe7Z1yYLqlGD6tBebrq/GFVRQ nico@Nasbert"
 }
 
-
 # Create a server
 resource "hcloud_server" "helloServer" {
   name         = "hello"
@@ -42,4 +42,6 @@ resource "hcloud_server" "helloServer" {
   datacenter   = "nbg1-dc3"
   firewall_ids = [hcloud_firewall.sshFw.id]
   ssh_keys     = [hcloud_ssh_key.loginDeniz.id, hcloud_ssh_key.loginNico.id, hcloud_ssh_key.loginGoik.id]
+  // New stuff here, Fig. 1001, Yaml file contains commands that should be run when the server is created
+  user_data = file("userData.yml")
 }
