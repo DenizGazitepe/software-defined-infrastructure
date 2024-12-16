9a26f644cefd0f670781dccc480b80837d25cf44
Deniz Gazitepe <dg102@hdm-stuttgart.de>
2024-12-16 17:31:36 +0100

    setup known hosts and add private network servers

--- task_11/main.tf
+++ task_11/main.tf
@@ -8,6 +8,12 @@ terraform {
   required_version = ">= 1.0"
 }
 
+resource "random_password" "root_password" {
+  length           = 16
+  special          = true
+  override_special = "!@#$%&*-_=+?"
+}
+
 # Setup the firewall
 resource "hcloud_firewall" "sshFw" {
   name = "ssh-firewall"
@@ -18,10 +24,20 @@ resource "hcloud_firewall" "sshFw" {
     source_ips = ["0.0.0.0/0", "::/0"]
   }
 }
+
+resource "tls_private_key" "host" {
+  algorithm = "ED25519"
+}
+
 # Add the ssh keys
 resource "hcloud_ssh_key" "loginDeniz" {
   name       = "deniz@LegionSlim7-Deniz"
   public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIHo157bpi8OVPIv9WPPfKlcWmCu+68E3Ii5nfjtarAU dg102@hdm-stuttgart.de"
+}
+
+resource "hcloud_ssh_key" "loginDenizWSL" {
+  name       = "deniz@LegionSlim7-WSL-Ubuntu"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICF0ll5wZPXwKvIqo9Dvp0bLJVu54pvHhji6i73pgiOE"
 }
 
 resource "hcloud_ssh_key" "loginGoik" {
@@ -34,23 +50,92 @@ resource "hcloud_ssh_key" "loginNico" {
   public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH9FnrukU1in+njcoOtPe7Z1yYLqlGD6tBebrq/GFVRQ nico@Nasbert"
 }
 
+resource "local_file" "ssh_script" {
+  content = templatefile("tpl/ssh.sh", {
+    ip = hcloud_server.helloServer.ipv4_address
+  })
+  filename        = "bin/ssh"
+  file_permission = "700"
+  depends_on      = [local_file.known_hosts]
+}
+
+resource "local_file" "known_hosts" {
+  content         = "${hcloud_server.helloServer.ipv4_address} ${tls_private_key.host.public_key_openssh}"
+  filename        = "gen/known_hosts"
+  file_permission = "644"
+}
+
 resource "hcloud_volume" "volume01" {
-  name = "volume1"
-  location = "nbg1"
-  size = 10
+  name      = "volume1"
+  location  = "nbg1"
+  size      = 10
   automount = false
-  format = "xfs"
+  format    = "xfs"
 }
 
 resource "local_file" "user_data" {
   content = templatefile("tpl/userData.yml", {
-    loginUser       = "devops"
-    volume_id=hcloud_volume.volume01.id
-    public_key_deniz = hcloud_ssh_key.loginDeniz.public_key
-    public_key_nico = hcloud_ssh_key.loginNico.public_key
-    public_key_goik = hcloud_ssh_key.loginGoik.public_key
+    loginUser           = "devops"
+    volume_id           = hcloud_volume.volume01.id
+    public_key_deniz    = hcloud_ssh_key.loginDeniz.public_key
+    public_key_denizWSL = hcloud_ssh_key.loginDenizWSL.public_key
+    public_key_nico     = hcloud_ssh_key.loginNico.public_key
+    public_key_goik     = hcloud_ssh_key.loginGoik.public_key
+    rootPassword        = random_password.root_password.result
+    tls_private_key     = indent(4, tls_private_key.host.private_key_openssh)
+    tls_public_key      = tls_private_key.host.public_key_openssh
   })
   filename = "gen/userData.yml"
+}
+
+resource "hcloud_network" "pNet" {
+  name     = "Private network"
+  ip_range = "10.0.0.0/8"
+}
+resource "hcloud_network_subnet" "pSubnet" {
+  network_id   = hcloud_network.pNet.id
+  type         = "cloud"
+  network_zone = "eu-central"
+  ip_range     = "10.0.1.0/24"
+}
+resource "hcloud_network_route" "gateway" {
+  network_id  = hcloud_network.pNet.id
+  destination = "0.0.0.0/0"
+  gateway     = "10.0.1.20"
+}
+
+resource "hcloud_server" "intern1" {
+  name        = "intern1"
+  image       = "debian-12"
+  server_type = "cx22"
+  location    = "nbg1"
+  ssh_keys    = [hcloud_ssh_key.loginDeniz.id, hcloud_ssh_key.loginDenizWSL.id, hcloud_ssh_key.loginNico.id, hcloud_ssh_key.loginGoik.id]
+  user_data   = local_file.user_data.content
+  public_net {
+    ipv4_enabled = false
+    ipv6_enabled = false
+  }
+  network {
+    network_id = hcloud_network.pNet.id
+    ip         = "10.0.1.21"
+  }
+}
+
+resource "hcloud_server" "intern2" {
+  name        = "intern2"
+  image       = "debian-12"
+  server_type = "cx22"
+  location    = "nbg1"
+  ssh_keys    = [hcloud_ssh_key.loginDeniz.id, hcloud_ssh_key.loginDenizWSL.id, hcloud_ssh_key.loginNico.id, hcloud_ssh_key.loginGoik.id]
+  user_data   = local_file.user_data.content
+  public_net {
+    ipv4_enabled = false
+    ipv6_enabled = false
+  }
+  network {
+    network_id = hcloud_network.pNet.id
+    ip         = "10.0.1.22"
+  }
 }
 
 # Create a server
@@ -58,13 +143,17 @@ resource "hcloud_server" "helloServer" {
   name         = "hello"
   image        = "debian-12"
   server_type  = "cx22"
-  location = "nbg1"
+  location     = "nbg1"
   firewall_ids = [hcloud_firewall.sshFw.id]
-  ssh_keys     = [hcloud_ssh_key.loginDeniz.id, hcloud_ssh_key.loginNico.id, hcloud_ssh_key.loginGoik.id]
+  ssh_keys     = [hcloud_ssh_key.loginDeniz.id, hcloud_ssh_key.loginDenizWSL.id, hcloud_ssh_key.loginNico.id, hcloud_ssh_key.loginGoik.id]
   user_data    = local_file.user_data.content
+  network {
+    network_id = hcloud_network.pNet.id
+    ip         = "10.0.1.20"
+  }
 }
 
 resource "hcloud_volume_attachment" "main" {
-  volume_id=hcloud_volume.volume01.id
-  server_id=hcloud_server.helloServer.id
+  volume_id = hcloud_volume.volume01.id
+  server_id = hcloud_server.helloServer.id
 }
